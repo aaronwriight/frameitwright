d7df7bf699569105394c4d25327a4b411642b828
aaronwriight <aaron.anthonyva@gmail.com>
2025-12-25 15:56:52 -0500

    fix: import VisualEditing from @sanity/visual-editing

--- src/components/live-visual-editing/index.tsx
+++ src/components/live-visual-editing/index.tsx
@@ -3,7 +3,7 @@
 'use client'
 
 import { useLiveMode } from '@sanity/react-loader'
-import { VisualEditing } from 'next-sanity'
+import { VisualEditing } from '@sanity/visual-editing'
 import { useEffect } from 'react'
 
 import { client } from '@/sanity/lib/client'

f4fa0316045db994d980c59636a39eafe839e94b
aaronwriight <aaron.anthonyva@gmail.com>
2025-12-25 17:34:29 -0500

    update about page again

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function about() {
   return (
     <main className="prose prose-stone dark:prose-invert prose-base container mx-auto  flex-1 space-y-7 px-6 py-11">
-      <h1 className="m-0 text-sm font-medium antialiased"><b>About</b></h1>
+      <h1 className="m-0 text-sm font-medium antialiased"><b>about</b></h1>
       <div>
         <h2 className="m-0  text-sm  font-medium antialiased">bio</h2>
         <p className="m-0 mt-2 text-sm  antialiased">

c89c66d74bc7f1ea370f2a6a0dad69c286ede2f1
aaronwriight <aaron.anthonyva@gmail.com>
2026-09-14 11:22:06 -0400

    Inset mobile homepage quote

--- src/components/guides/guides.tsx
+++ src/components/guides/guides.tsx
@@ -58,7 +58,7 @@ export default function Guides({ guides }: { guides: SanityGuide[] }) {
       })}
         </section>
       </div>
-      <p className="ml-auto mt-16 w-full max-w-3xl text-right font-serif text-xs italic leading-5 sm:mt-14 sm:w-auto sm:text-sm sm:leading-relaxed md:absolute md:-bottom-16 md:right-6 md:mt-0">
+      <p className="ml-4 mt-16 w-[calc(100%-1rem)] max-w-3xl text-right font-serif text-xs italic leading-5 sm:ml-auto sm:mt-14 sm:w-auto sm:text-sm sm:leading-relaxed md:absolute md:-bottom-16 md:right-6 md:mt-0">
         Well, that is one of the things to find out sometime.
         <br />
         Isn&apos;t it splendid to think of all the things there are to find out about?

690184fc92c2851d046830711d09c392a1223704
aaronwriight <aaron.anthonyva@gmail.com>
2026-09-14 11:42:46 -0400

    Standardize full-width journal posts

--- src/app/(site)/scope-for-imagination/[slug]/page.tsx
+++ src/app/(site)/scope-for-imagination/[slug]/page.tsx
@@ -68,7 +68,7 @@ export default async function ScopeForImaginationEntryPage({ params }: { params:
           <SfiPostHeader post={post} large className="mt-10 border-b border-stone-300 pb-7 dark:border-stone-700" />
         </div>
 
-        <div className="prose prose-stone mt-10 w-full max-w-3xl font-serif text-sm leading-7 dark:prose-invert prose-headings:font-serif prose-a:text-[#6f8200] prose-img:my-10 prose-img:w-full">
+        <div className="prose prose-stone mt-10 w-full max-w-none font-serif text-sm leading-7 dark:prose-invert prose-headings:font-serif prose-a:text-[#6f8200] prose-img:my-10 prose-img:w-full">
           {post.body ? (
             <PortableText value={post.body} components={myPortableTextComponents} />
           ) : (
